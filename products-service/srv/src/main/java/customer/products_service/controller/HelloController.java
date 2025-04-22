1d5283c29e03cc6e5c8bf4624b70036be49605e9
Shinya Araki <shinya.araki@akkodis.co.jp>
2025-04-22 02:32:58 +0000

    Initial Commit @4/22/2025, 2:32:58 AM

---
+++ products-service/srv/src/main/java/customer/products_service/controller/HelloController.java
@@ -0,0 +1,17 @@
+package customer.products_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, CI/CD!";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "Test2, CI/CD!";
+    }
+}
